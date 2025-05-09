7315df9703fc73daf58f88f87fb4b96a562c4e4d
byviusk <valopez@scl.edu.gt>
2025-05-09 03:10:23 -0600

    Edicion de codigo y sistema de carpetas

---
+++ Main.java
@@ -0,0 +1,22 @@
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+
+public class Main {
+
+    public static void main(String[] args) {
+        
+        // Crear una ventana con JFrame
+        JFrame miVentana = new JFrame();
+        miVentana.setTitle("Hospital SCL");     // Establece el título de la ventana
+        miVentana.setSize(1000, 800);           // Establece el tamaño de la ventana (ancho x alto
+    
+        // Crear un texto
+        JLabel texto = new JLabel();
+        texto.setText("Hola mundo");            // Establece el texto del JLabel
+
+        miVentana.add(texto);                   // Añade el JLabel a la ventana (usa el layout por defecto)
+
+        // Hacer visible la ventana
+        miVentana.setVisible(true);             // Muestra la ventana en pantalla
+    }
+}
